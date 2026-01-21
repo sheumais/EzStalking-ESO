6d78a217ffb71485d28c7d7fcba5c961d0450c6f
sheumais <88064574+sheumais@users.noreply.github.com>
2026-01-21 19:13:56 +1100

    Fix Settings for Adventure Zone Toggle

--- EzStalkingMenu.lua
+++ EzStalkingMenu.lua
@@ -249,13 +249,18 @@ function Ez.Menu:initialize()
     }
     options_table[#options_table+1] =
     {
+        type = "divider",
+        width = "half",
+    }
+    options_table[#options_table+1] =
+    {
         type = "checkbox",
         width = "half",
         name = L.menu.location.adventure,
         tooltip = L.menu.location.adventure_tooltip,
         getFunc = function() return Ez.settings.log.adventure end,
         setFunc = function(value) Ez.settings.log.adventure = value end,
-        disabled = function() return not Ez.settings.log.adventure end,
+        disabled = function() return not Ez.settings.log.enabled end,
         requiresReload = false,
         default = Ez.defaults.log.adventure,
     }
